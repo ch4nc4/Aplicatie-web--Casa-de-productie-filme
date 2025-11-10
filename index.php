3fe367f62358f305d2d1b9cef9efbc132464a931
ch4nc4 <raluca-melinda.ionete@s.unibuc.ro>
2025-11-10 21:48:16 +0200

    am redactat descrierea aplicatiei si a bazei de date aferente ei

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ro">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Casa de productie filme independenta </title>
+</head>
+
+<body>
+    <div class="container">
+        <h1> Descrierea Aplicatiei Web </h1>
+
+        <br>
+        <br>
+
+        <ul>
+            <li>
+                <h2> Scopul aplicatiei </h2>
+                <p> Aplicatia isi propune sa prezinte proiectele in curs de dezvoltare, <br>
+                dar si cele finalizate, ale unei case de productie de film independente.<br><br>
+                Utilizatorul va avea oportunitatea sa cunoasca viizunea companiei, componenta echipei de productie <br>
+                si principalii actori de se afla in contract cu aceasta casa de productie. </p><br>
+            </li>
+            <li>
+                <h2> Roluri valabile la accesarea aplicatiei </h2>
+                <ul>
+                    <li> Administrator pagina web </li>
+                    <li> Utilizator extern </li>
+                    <li> Lider echipa de productie  </li>
+                    <li> Staff echipa de productie (lumina, sunet, video) </li>
+                    <li> Lider creativitate (viziune artistica) </li>
+                    <li> Staff echipa creativitate (scenografie, vizual) </li>
+                    <li> Lider finante </li>
+                </ul>
+            </li>
+        </ul>
+
+
